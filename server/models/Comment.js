6f7e67d1c66657e2846dae87e4a37441fbfae8af
Derek Shain <derek0anthony@gmail.com>
2021-09-17 14:33:36 -0600

    comments server

--- server/models/Comment.js
+++ server/models/Comment.js
@@ -3,6 +3,7 @@ const Schema = mongoose.Schema
 export const CommentSchema = new Schema({
   comment: { type: String, required: true },
   creatorId: { type: Schema.Types.ObjectId, ref: 'Account', required: true }
+  postId: {}
 },
 { timestamps: true, toJSON: { virtuals: true } }
 )
