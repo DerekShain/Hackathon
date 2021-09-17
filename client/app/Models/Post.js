7f9bbe5495095069bef7ab792333e85cd8e897a8
Derek Shain <derek0anthony@gmail.com>
2021-09-17 15:11:12 -0600

    changes

--- client/app/Models/Post.js
+++ client/app/Models/Post.js
@@ -12,14 +12,13 @@ export class Post {
 
   get Template() {
     return /* html */ `
-    <div class="card m-2 masonry-with-columns" style="width: 18rem; background: #F19953">
+    <div class="card m-2 justify-content-start" style="background: #F19953">
   <img src="${this.imgUrl}" class="card-img-top" alt="...">
-  <div class="card-body">
+
     <h5 class="card-title">${this.name}</h5>
     <i class="fas fa-trash-alt ${this.creatorId !== ProxyState.account.id ? 'visually-hidden' : ''} selectable" onclick="app.postsController.deletePost('${this.id}')"></i>
     </div>
-    </div>
-  </div>
+
 `
   }
 }
