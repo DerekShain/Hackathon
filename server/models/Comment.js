5e858c96725c458e01bc1c93aba5cafeab2cc899
Derek Shain <derek0anthony@gmail.com>
2021-09-17 16:47:54 -0600

    Navbar

--- server/models/Comment.js
+++ server/models/Comment.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema
 export const CommentSchema = new Schema({
   comment: { type: String, required: true },
   creatorId: { type: Schema.Types.ObjectId, ref: 'Account', required: true },
-  postId: { type: mongoose.Schema.Types.ObjectId }
+  postId: { type: mongoose.Schema.Types.ObjectId, required: true }
 },
 { timestamps: true, toJSON: { virtuals: true } }
 )
