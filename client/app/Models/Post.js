c7e7a99113f9b55131d288e2f2a1c2771aaf6111
Quinnton <quinnclimbs@gmail.com>
2021-09-17 15:41:19 -0600

    commit

--- client/app/Models/Post.js
+++ client/app/Models/Post.js
@@ -12,8 +12,8 @@ export class Post {
 
   get Template() {
     return /* html */ `
-    <div class="card m-2 masonry-with-columns" style="background: #F19953" data-bs-toggle="modal" data-bs-target="#exampleModal">
-  <img src="${this.imgUrl}" class="card-img-top" alt="..." onclick="app.postsController.drawModal('${this.id}', '${this.creatorId}')">
+    <div class="card m-2 masonry-with-columns" style="background: #F19953">
+  <img src="${this.imgUrl}" class="card-img-top" alt="..." onclick="app.postsController.drawModal('${this.id}', '${this.creatorId}')" data-bs-toggle="modal" data-bs-target="#exampleModal">
 
     <h5 class="card-title">${this.name}</h5>
     <i class="fas fa-trash-alt ${this.creatorId !== ProxyState.account.id ? 'visually-hidden' : ''} selectable" onclick="app.postsController.deletePost('${this.id}')"></i>
