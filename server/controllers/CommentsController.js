d4cdee089ac56e9913f2aa6437cbdf7cec508ab4
Jeremy Fowler <jembyfowler@gmail.com>
2021-09-17 15:40:32 -0600

    comment working on server

--- server/controllers/CommentsController.js
+++ server/controllers/CommentsController.js
@@ -1,18 +1,51 @@
+import { Auth0Provider } from '@bcwdev/auth0provider'
+import { commentsService } from '../services/CommentsService'
 import BaseController from '../utils/BaseController'
 
 export class CommentsController extends BaseController {
   constructor() {
-    super('api/comments')
+    super('api/posts/:id/comments')
     this.router
       .get('', this.getComments)
-      .get('', this.getComments)
-      .
+      .get('/:id', this.getComment)
+      .use(Auth0Provider.getAuthorizedUserInfo)
+      .post('', this.addComment)
+      .delete('/:id', this.deleteComment)
   }
-  getComments() {
+
+  async addComment(req, res, next) {
     try {
-      
+      req.body.creatorId = req.userInfo.id
+      const comment = await commentsService.addComment(req.body)
+      res.send(comment)
     } catch (error) {
-      
+      next(error)
+    }
+  }
+
+  async deleteComment(req, res, next) {
+    try {
+
+    } catch (error) {
+
+    }
+  }
+
+  async getComment(req, res, next) {
+    try {
+      const comment = await commentsService.getComment(req.params.id)
+      res.send(comment)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async getComments(req, res, next) {
+    try {
+      const comments = await commentsService.getComments(req.query)
+      res.send(comments)
+    } catch (error) {
+      next(error)
     }
   }
 }
--- server/models/Comment.js
+++ server/models/Comment.js
@@ -2,8 +2,8 @@ import mongoose from 'mongoose'
 const Schema = mongoose.Schema
 export const CommentSchema = new Schema({
   comment: { type: String, required: true },
-  creatorId: { type: Schema.Types.ObjectId, ref: 'Account', required: true }
-  postId: {}
+  creatorId: { type: Schema.Types.ObjectId, ref: 'Account', required: true },
+  postId: { type: mongoose.Schema.Types.ObjectId }
 },
 { timestamps: true, toJSON: { virtuals: true } }
 )
